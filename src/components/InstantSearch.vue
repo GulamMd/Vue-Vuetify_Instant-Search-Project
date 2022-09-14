b6d7eb31a11ba3aebd2c8a1a043d06a94a7cc86a
gulam Khalid <gulam.khalid@globalids.com>
2022-09-14 14:12:42 +0530

    Implemented backend i.e data is coming from express api

--- src/components/InstantSearch.vue
+++ src/components/InstantSearch.vue
@@ -33,8 +33,8 @@
                     <v-list-item-title>
                       <span>{{ item.title }}</span>
                     </v-list-item-title>
-                    <v-btn icon target="_blank" :href="item.url"> 
-                      <font-awesome-icon icon="fas fa-external-link-alt"/>
+                    <v-btn icon target="_blank" :href="item.url">
+                      <font-awesome-icon icon="fas fa-external-link-alt" />
                     </v-btn>
                   </div>
                 </v-list-item-content>
@@ -49,48 +49,13 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "HelloWorld",
 
   data: () => ({
     searchText: "",
-    articles: [
-      {
-        title: "What You Need To Know About CSS Variables",
-        url: "https://tutorialzine.com/2016/03/what-you-need-to-know-about-css-variables/",
-        image: "https://tutorialzine.com/media/2016/03/css-variables.jpg",
-      },
-      {
-        title: "Freebie: 4 Great Looking Pricing Tables",
-        url: "https://tutorialzine.com/2016/02/freebie-4-great-looking-pricing-tables/",
-        image:
-          "https://tutorialzine.com/media/2016/02/great-looking-pricing-tables.jpg",
-      },
-      {
-        title: "20 Interesting JavaScript and CSS Libraries for February 2016",
-        url: "https://tutorialzine.com/2016/02/20-interesting-javascript-and-css-libraries-for-february-2016/",
-        image:
-          "https://tutorialzine.com/media/2016/02/interesting-resources-february.jpg",
-      },
-      {
-        title: "Quick Tip: The Easiest Way To Make Responsive Headers",
-        url: "https://tutorialzine.com/2016/02/quick-tip-easiest-way-to-make-responsive-headers/",
-        image:
-          "https://tutorialzine.com/media/2016/02/quick-tip-responsive-headers.png",
-      },
-      {
-        title: "Learn SQL In 20 Minutes",
-        url: "https://tutorialzine.com/2016/01/learn-sql-in-20-minutes/",
-        image:
-          "https://tutorialzine.com/media/2016/01/learn-sql-20-minutes.png",
-      },
-      {
-        title: "Creating Your First Desktop App With HTML, JS and Electron",
-        url: "https://tutorialzine.com/2015/12/creating-your-first-desktop-app-with-html-js-and-electron/",
-        image:
-          "https://tutorialzine.com/media/2015/12/creating-your-first-desktop-app-with-electron.png",
-      },
-    ],
+    articles: [],
     tempArr: [],
   }),
   watch: {
@@ -101,6 +66,9 @@ export default {
       },
       immediate: true,
     },
+  },
+  async mounted() {
+    this.getApi();
   },
   methods: {
     searchInternal() {
@@ -120,6 +88,10 @@ export default {
     },
     updateSearchTxtFun() {
       this.tempArr = this.articles;
+    },
+    async getApi() {
+      let apiData = await axios.get("http://localhost:5000/users");
+      this.articles = apiData.data;
     },
   },
 };
